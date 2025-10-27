d69f3c59fed8f9da19cfbe85c3550625d13b1e97
Xuân Lâm <hoangxuanlam2007@outlook.com>
2025-10-27 10:32:43 +0700

    Declared Typescript CSS Module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "@/app/globals.css"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
